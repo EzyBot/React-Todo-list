dd472b7ba56afb7d06f55f06556cafb5e6066c56
A Harshith <hanoo@hsdyn.com>
2023-08-28 11:31:24 +0530

    todo-list push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import InputControl from './Components/InputTODO'
+import List from './Components/ListTODO';
 
 function App() {
+  const [listTodo,setListTodo]=useState([]);
+  let addList = (inputText)=>{
+    if(inputText!=='')
+      setListTodo([...listTodo,inputText]);
+      console.log(listTodo);
+  }
+  const deleteListItem = (index)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(index,1)
+    setListTodo([...newListTodo])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="center-container">
+        <InputControl addList={addList}/>
+        <h1 className="app-heading">TODO</h1>
+        <hr/>
+        {listTodo.map((listItem,i)=>{
+          return (
+            <List key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
